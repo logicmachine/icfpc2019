9901122fa5a5594e12b0eab7353d259010261111
T.Ikeda <ti11192916@gmail.com>
2019-06-21 21:59:54 +0900

    入力データのスクリーンショットを撮るやつ

---
+++ ikeda/getSS.py
@@ -0,0 +1,26 @@
+import time
+import os.path
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+options = Options()
+options.binary_location = '/usr/bin/google-chrome'
+options.add_argument('--headless')
+options.add_argument('--window-size=1280,1024')
+
+driver = webdriver.Chrome('chromedriver', chrome_options=options)
+driver.get('https://icfpcontest2019.github.io/solution_visualiser/')
+
+# visualizer DOM?
+element = driver.find_element_by_id('submit_task') 
+
+input_data_path = './desc'
+files = []
+for filename in os.listdir(input_data_path):
+    print(filename)
+    element.send_keys(os.path.abspath(input_data_path) + '/' + filename)
+    time.sleep(5)
+    driver.save_screenshot('./screenshot/' + filename + '.png')
+
+driver.quit()
