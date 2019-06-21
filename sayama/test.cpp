cae65b0ce1ea39b31794e35f93dfd62101c623c5
Yuichi Sayama <yuichi.sayama@fixtars.com>
2019-06-21 23:48:52 +0900

    finishgit status!

---
+++ sayama/test.cpp
@@ -0,0 +1,24 @@
+#include "board_loader.hpp"
+
+#include <iostream>
+
+using boardloader::Cell;
+using boardloader::load;
+using boardloader::print_table;
+
+using namespace std;
+
+int main()
+{
+    int start_y, start_x;
+    auto table = load("/home/xyz600/program/contest/icfpc/2019/example/example-01.desc", start_y, start_x);
+
+    std::cout << "start_y = " << start_y << endl;
+    std::cout << "start_x = " << start_x << endl;
+
+    Cell cell = Cell::Empty;
+
+    cout << table[0][0] << endl;
+
+    print_table(table, start_y, start_x);
+}
