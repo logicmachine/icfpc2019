d088b8ab3747354dc1337f1e074fb1a4cd7e1d4f
T.Ikeda <ti11192916@gmail.com>
2019-06-22 05:34:44 +0900

    update header

--- ikeda/ikeda.hpp
+++ ikeda/ikeda.hpp
@@ -1,40 +1,8 @@
 #pragma once
 
-#include <bits/stdc++.h>
 #include "../sayama/board_loader.hpp"
 
 using namespace std;
-
-#define INF 1001000100010001000
-#define MOD 1000000007
-#define EPS 1e-10
-#define rep(i, N) for (int i = 0; i < N; i++)
-#define Rep(i, N) for (int i = 1; i < N; i++)
-#define For(i, a, b) for (int i = (a); i < (b); i++)
-#define pb push_back
-#define eb emplace_back
-#define mp make_pair
-#define pii pair<int, int>
-#define vi vector<int>
-#define vvi vector<vi >
-#define vb vector<bool>
-#define vvb vector<vb >
-#define vp vector< pii >
-#define all(a) (a).begin(), (a).end()
-#define Int(x) int x; cin >> x;
-#define int2(x, y) Int(x); Int(y);
-#define int3(x, y, z) Int(x); int2(y, z);
-#define in(x, a, b) ((a) <= (x) && (x) < (b))
-#define fir first
-#define sec second
-#define ffir first.first
-#define fsec first.second
-#define sfir second.first
-#define ssec second.second
-#define Decimal fixed << setprecision(10)
-
-//int dxy[5] = {0, 1, 0, -1, 0};
-// 01Trie BIT CHT DFT FFT avl cmd cusum dijkstra dinic geo2 gin graph kruskal lca lcm matrix ncm next_combination ppuf segtree st tmp topcoder uf vi 
 
 namespace ikeda
 {
