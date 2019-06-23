15fa7e7265263cb9efeb0215add525ab51ef3432
Takaaki Hiragushi <ent@arrize.net>
2019-06-23 22:06:12 +0900

    [web] booster purchasing

--- server/app.py
+++ server/app.py
@@ -108,9 +108,10 @@ def submit():
         problem_id = int(request.form['problem_id'])
         solution = request.form['solution']
         author = request.form['author']
+        boosters = '' if 'boosters' not in request.form else request.form['boosters']
         r = requests.post(
             'http://localhost:5001/verify/{}'.format(problem_id),
-            {'solution': solution})
+            {'solution': solution, 'boosters': boosters})
         if r.status_code == 404:
             success = False
             message = 'Problem not found'
@@ -119,20 +120,22 @@ def submit():
             success = response['success']
             message = response['message']
             if success:
-                query = 'insert into solutions (problem_id, score, content, author) values (?, ?, ?, ?)'
-                conn.execute(query, (problem_id, response['score'], solution, author))
+                query = 'insert into solutions (problem_id, score, content, author, buys) values (?, ?, ?, ?, ?)'
+                conn.execute(query, (problem_id, response['score'], solution, author, boosters))
                 conn.commit()
     return render_template('submit.html', success=success, message=message)
 
 @app.route('/make-zip')
 def make_zip():
     conn = get_db()
-    query = 'select problem_id, min(score), content from solutions group by problem_id'
+    query = 'select problem_id, min(score), content, buys from solutions group by problem_id'
     response = make_response()
     with TemporaryFile() as tf, ZipFile(tf, mode='w') as zf:
         for row in conn.execute(query):
             name = 'prob-{:03d}.sol'.format(row['problem_id'])
             zf.writestr(name, row['content'])
+            name = 'prob-{:03d}.buy'.format(row['problem_id'])
+            zf.writestr(name, row['buys'])
         zf.close()
         tf.seek(0)
         response.data = tf.read()
