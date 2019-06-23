bc22f408d54c4b68de1c781799d011d614477089
Yuichi Sayama <yuichi.sayama@fixtars.com>
2019-06-24 00:25:50 +0900

    update submit script

--- sayama/submit.sh
+++ sayama/submit.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 folder=local-judge/answer/submit9
-prob_to=300
 prob_from=$1
 mod=$2
+prob_to=$3
 
 for i in `seq $prob_from $mod $prob_to`;
 do
--- sayama/submit_all.sh
+++ sayama/submit_all.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 num_parallel=7
+start_id=$1
+end_id=$2
+
 for i in `seq 1 $num_parallel`;
 do
-    ./submit.sh $i $num_parallel &
+    offset=$(( i + start_id ))
+    ./submit.sh $offset $num_parallel $end_id &
 done
