788ad45dc8b16a3a0fb9d261f1c68ef7733481dc
Takaaki Hiragushi <ent@arrize.net>
2019-06-24 01:01:55 +0900

    popt-runner with boosters

--- logicmachine/popt-runner/main.py
+++ logicmachine/popt-runner/main.py
@@ -1,4 +1,5 @@
 import os
+import re
 import subprocess
 import requests
 
@@ -7,37 +8,68 @@ from joblib import Parallel, delayed
 
 API_SERVER = 'http://icfpc.logicmachine.jp:5000'
 
+
+def get_problems():
+    r = requests.get('{}/index.json'.format(API_SERVER))
+    problems = r.json()
+    return [p['problem_id'] for p in problems]
+
+def get_submissions(problem_id):
+    r = requests.get('{}/details_json/{}'.format(API_SERVER, problem_id))
+    raw_submissions = r.json()
+    submissions = dict()
+    best_score = raw_submissions[0]['score']
+    for s in raw_submissions:
+        if s['score'] > best_score * 1.5:
+            continue
+        prefix = re.match(r'[a-zA-Z]+', s['author']).group(0)
+        if prefix in submissions:
+            pass
+        submissions[prefix] = {
+            'problem_id': problem_id,
+            'submission_id': s['id'],
+            'author': s['author']
+        }
+    return submissions.values()
+
 def process(info):
+    problem_id = info['problem_id']
+    submission_id = info['submission_id']
     author = info['author']
-    # if author.find('+popt') >= 0:
-    #     return
-    problem = requests.get('{}/problems/{}'.format(API_SERVER, info['problem_id'])).text
-    solution = requests.get('{}/download/{}'.format(API_SERVER, info['submission_id'])).text
-    with NamedTemporaryFile() as pf, NamedTemporaryFile() as sf:
+    problem = requests.get('{}/problems/{}'.format(API_SERVER, problem_id)).text
+    solution = requests.get('{}/download/{}'.format(API_SERVER, submission_id)).text
+    boosters = requests.get('{}/boosters/{}'.format(API_SERVER, submission_id)).text
+    with NamedTemporaryFile() as pf, NamedTemporaryFile() as sf, NamedTemporaryFile() as bf:
         pf.write(problem.encode('utf-8'))
         pf.flush()
         sf.write(solution.encode('utf-8'))
         sf.flush()
+        bf.write(boosters.encode('utf-8'))
+        bf.flush()
         proc = subprocess.Popen(
-            ['../patch-optimizer/a.out', pf.name, sf.name],
+            ['../patch-optimizer/a.out', pf.name, sf.name, bf.name],
             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         outs, errs = proc.communicate()
         if proc.returncode != 0:
-            print('Failed to optimize {} (problem {})'.format(info['submission_id'], info['problem_id']))
+            print('Failed to optimize {} (problem {})'.format(submission_id, problem_id))
         else:
             solution = outs.decode('utf-8')
             r = requests.post(
                 '{}/submit'.format(API_SERVER),
                 {
-                    'problem_id': info['problem_id'],
-                    'author': info['author'] + '+popt7',
-                    'solution': solution
+                    'problem_id': problem_id,
+                    'author': author + '+popt7',
+                    'solution': solution,
+                    'boosters': boosters
                 })
 
 def main():
-    r = requests.get('{}/index.json'.format(API_SERVER))
-    submissions = r.json()
-    Parallel(n_jobs=-1, verbose=5)([delayed(process)(p) for p in submissions])
+    problems = get_problems()
+    submissions = []
+    for problem in problems:
+        submissions.extend(get_submissions(problem))
+    print('Optimize {} submissions'.format(len(submissions)))
+    Parallel(n_jobs=-1, verbose=9)([delayed(process)(p) for p in submissions])
 
 if __name__ == '__main__':
     main()
